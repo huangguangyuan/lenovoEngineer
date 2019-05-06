036040382c598de44ec92171fa8707f8bc435ce8
unknown <Huangguangyuan@gdsunfly.com>
2019-05-06 17:15:05 +0800

    【修改】订单详情

--- src/views/orderDetails.vue
+++ src/views/orderDetails.vue
@@ -1,123 +1,39 @@
 <template>
-  <div class="orderList">
-    <van-nav-bar title="服务管理" fixed border />
-    <van-tabs type="card">
-      <van-tab title="未完成">
-        <ul class="list">
-          <li v-for='item in noFinishList'>
-            <p>
-              <span>客户姓名：</span>
-              <span>{{item.username}}</span>
-            </p>
-            <p>
-              <span>电 话：</span>
-              <a :href="'tel:'+item.phone">
-                <van-icon name="phone" /> 拨号
-              </a>
-            </p>
-            <p>
-              <span>订单号：</span>
-              <span>{{item.orders_id}}</span>
-            </p>
-            <p>
-              <span>预约时间：</span>
-              <span>{{item.appoint_time}}</span>
-            </p>
-            <p>
-              <span>{{item.install_type==1?'安装地址：':'维修地址：'}}</span>
-              <span>{{item.install_address}}</span>
-            </p>
-            <van-button plain type="primary" size='small'>未接单</van-button>
-          </li>
-        </ul>
-      </van-tab>
-      <van-tab title="已完成">
-        <ul class="list">
-          <li v-for='item in finishList'>
-            <p>
-              <span>客户姓名：</span>
-              <span>{{item.username}}</span>
-            </p>
-            <p>
-              <span>电 话：</span>
-              <span>{{item.phone}}</span>
-            </p>
-            <p>
-              <span>订单号：</span>
-              <span>{{item.orders_id}}</span>
-            </p>
-            <p>
-              <span>预约时间：</span>
-              <span>{{item.appoint_time}}</span>
-            </p>
-            <p>
-              <span>安装地址：</span>
-              <span>{{item.install_address}}</span>
-            </p>
-            <van-button plain type="primary" size='small'>未接单</van-button>
-          </li>
-        </ul>
-      </van-tab>
-    </van-tabs>
+  <div class="orderDetails">
+    <van-nav-bar title="订单详情" fixed border left-arrow/>
+    <div class="banner">
+      未完成
+    </div>
+    <div class="message">
+      <p>
+        <span></span>
+        <span></span>
+      </p>
+    </div>
   </div>
 </template>
 <script>
 export default {
-  name: "orderList",
+  name: "orderDetails",
   data() {
     return {
-      tabNav:['未完成','已完成'],
-      noFinishList:[],//未完成数据列表
-      finishList:[],//已完成数据列表
+      
     };
   },
   mounted(){
-    this.getNoFinishList();
-    this.getFinishList();
+    
   },
   methods:{
-    // 获取未完成列表
-    getNoFinishList(){
-      var _this = this;
-      var reqUrl = '/index/installer/getUnfinishOrderList';
-      _this.$http.post(reqUrl,{}).then(res => {
-        console.log(res.data.data);
-        _this.noFinishList = res.data.data;
-      })
-    },
-    // 获取已完成列表
-    getFinishList(){
-      var _this = this;
-      var reqUrl = '/index/installer/getFinishOrderList';
-      _this.$http.get(reqUrl,{}).then(res => {
-        console.log(res);
-        _this.finishList = res.data.data;
-      })
-    }
+    
   }
 };
 </script>
 
 <style scoped lang="scss">
-.orderList{
+.orderDetails{
     position: absolute;width: 100%;height: 100%;top:0;left: 0;box-sizing: border-box;background-color: #ededed;padding-top:46px;
-    .van-nav-bar{z-index: 99;}
-    .van-tabs{
-      margin-top: 20px;
-    }
-    .list{
-      li{
-        padding:13px;background-color: #ffffff;position: relative;margin: 10px auto 10px;
-        p{
-          display: flex;font-size: 14px;margin-top: 6px;justify-content: flex-start;align-items: center;
-          span:nth-child(1){width: 80px;text-align: right;}
-          span:nth-child(2){width: 205px;overflow:hidden;text-overflow:ellipsis;white-space:nowrap}
-          a{padding:3px 10px;background-color: #f7f7f7;border:1px #ebebeb solid;border-radius: 8px;color: #8fc31f;font-size: 14px;display: flex;justify-content: center;align-items: center;}
-        }
-        .van-button{position: absolute;bottom: 13px;right: 13px;}
-      }
-      
-    }
+    .banner{width: 375px;height: 75px;margin: 0 auto;background-color: #ff9600;text-align: center;font-size: 25px;color: #ffffff;line-height: 75px;}
+    .message{width: 375px;box-sizing: border-box;padding: 20px 13px;border-top:1px #f0f0f0 solid;border-bottom:1px #f0f0f0 solid;}
 }
 </style>
 
